e80881c027108cbbccfac394813d0166fe9a4d12
karifl <lampi_88@hotmail.com>
2020-04-06 20:53:42 +0300

    Update Main.java

--- templateMethod/Main.java
+++ templateMethod/Main.java
@@ -11,35 +11,5 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		RussianRoulette rr = new RussianRoulette();
-		rr.initializeGame();
-	/*	
-	 * 
-	 * 
-	 * int bullet= 1;
-		int chamber = 1;
-		int maxChambers=6;
-		int minChambers =1;
-		
-	 * 
-	 * 
-		bullet =(int) (Math.random()* ((maxChambers - minChambers)+1)+minChambers); //initialize the bullet
-		chamber = (int) (Math.random()* ((maxChambers - minChambers)+1)+minChambers);		//initalize the starting chamber
-		
-		System.out.println("bullet: " + bullet);
-		System.out.println("chamber: " + chamber);
-
-		bullet =(int) (Math.random()* ((maxChambers - minChambers)+1)+minChambers); //initialize the bullet
-		chamber = (int) (Math.random()* ((maxChambers - minChambers)+1)+minChambers);		//initalize the starting chamber
-		
-		System.out.println("bullet: " + bullet);
-		System.out.println("chamber: " + chamber);
-		
-		bullet =(int) (Math.random()* ((maxChambers - minChambers)+1)+minChambers); //initialize the bullet
-		chamber = (int) (Math.random()* ((maxChambers - minChambers)+1)+minChambers);		//initalize the starting chamber
-		
-		System.out.println("bullet: " + bullet);
-		System.out.println("chamber: " + chamber);
-		*/
-	}
-
+		rr.playOneGame(2);
 }
